46ee9fdd311c87c2754fe95b1b1c4460e590a428
Sandip Rijal <sandurijal03@hotmail.com>
2021-06-05 03:22:16 +0545

    initil commit and refactoring cra template

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Topbar from './components/topbar/Topbar'
+
+const App = () => {
+    return(
+        <div>
+            <Topbar />
+        </div>
+    )
+}
+
+export default  App
