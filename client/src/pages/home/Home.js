0e3698c13e1a5bcd58277fdae75a7b4a3270e2a6
Sandip Rijal <sandurijal03@hotmail.com>
2021-06-05 06:50:32 +0545

    implemented chart

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
+
+import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import styles from './Home.module.css';
+import { userData } from '../../dumData';
 
 const Home = () => {
   return (
     <div className={styles.home}>
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title='User Analytics'
+        grid
+        datakey='Active User'
+      />
     </div>
   );
 };
