9e5e4a2f339c09d3725e3514b4ea97b6be1daf68
Sandip Rijal <sandurijal03@hotmail.com>
2021-06-06 05:24:58 +0545

    implemented in app the product and productlist page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
 import NewUser from './pages/newUser/NewUser';
+import Product from './pages/product/Product';
+import ProductList from './pages/productList/ProductList';
 import User from './pages/user/User';
 import UserList from './pages/userList/UserList';
 
@@ -27,6 +29,12 @@ const App = () => {
           </Route>
           <Route path='/newuser'>
             <NewUser />
+          </Route>
+          <Route path='/products'>
+            <ProductList />
+          </Route>
+          <Route path='/product/:productId'>
+            <Product />
           </Route>
         </Switch>
       </div>
